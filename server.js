88682d06928f3561971aa085a19e86dc5d7567e0
unknown <Puinun>
2018-10-19 13:00:57 +0700

    test3

--- server.js
+++ server.js
@@ -108,10 +108,10 @@ app.post('/product/update',function (req, res) {
     var sql = `Update product set title = ${title}, price = ${price} where id = ${id}`;
   //  var sql = 'Update product set title = "'+ title +'", price = "'+ price + '" where id = '+ id;
 // db.none
-app.get('/add', function (request, response) {
-    var time = moment().format('MMMM Do YYYY, h:mm:ss a');
-    response.render('pages/insert', { time: time});
+app.get('/products/add', function (req, res) {
+    res.render('pages/index');
 });
+
 console.log('UPDATE : '+ sql);
     res.redirect('/products');
     
