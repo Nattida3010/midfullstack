09ae2a60cc81c20891061b4700d345640cffea28
unknown <Puinun>
2018-10-19 12:01:51 +0700

    add buttot cancel delate add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var pgp = require('pg-promise')();
-var db =pgp(process.env.DATABASE_URL);
+//var db =pgp(process.env.DATABASE_URL);
 var db = pgp('postgres://sguzzrbjzwawcf:b9b96230384d50890bff7be60b7b32a703beca2827c9f83f3eaaa9c96f6ff251@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d9i6vbnk6jnrb4?ssl=true')
 var app = express();
 var bodyParser = require('body-parser');
@@ -25,7 +25,7 @@ app.get('/index', function (req, res) {
 });
 app.get('/about', function (req, res) {
     var name = 'Puinun';
-    var hobbies = ['Footba', 'Movie', 'Programming']
+    var hobbies = ['Football', 'Movie', 'Programming']
     var bdate = '06/08/1997'
     res.render('pages/about', { nickname: name, hobbies: hobbies, bdate: bdate });
 });
