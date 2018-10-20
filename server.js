1552d589f88af99fa85eaa87683c575c412b2251
unknown <Puinun>
2018-10-20 10:10:54 +0700

    editbutton

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.post('/product/update',function (req, res) {
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
-    var sql = `Update products set title = ${title}, price = ${price} where id = ${id}`;
+    var sql = `update products set title = '${title}', price = '${price}' where id = '${id}'`;
      //db.none
     db.any(sql)
         .then(function (data) {
